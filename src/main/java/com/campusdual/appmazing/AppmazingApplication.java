0c1d84bfdaaa07b89d0b04a65caed0f94092851c
ptaibo <ptaibog@gmail.com>
2023-09-25 09:03:31 +0200

    initial commit

---
+++ src/main/java/com/campusdual/appmazing/AppmazingApplication.java
@@ -0,0 +1,13 @@
+package com.campusdual.appmazing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppmazingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppmazingApplication.class, args);
+	}
+
+}
