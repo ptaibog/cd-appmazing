c7cc9da63ccbd9791789235e7089312c4976cb3c
ptaibo <ptaibog@gmail.com>
2023-09-26 12:22:57 +0200

    Add Contact DTO

---
+++ src/main/java/com/campusdual/appmazing/model/dto/ContactDTO.java
@@ -0,0 +1,64 @@
+package com.campusdual.appmazing.model.dto;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class ContactDTO {
+
+    private int id;
+    private String name;
+    private String firstsurname;
+    private String secondsurname;
+    private int phonenumber;
+    private String mail;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFirstsurname() {
+        return firstsurname;
+    }
+
+    public void setFirstsurname(String firstsurname) {
+        this.firstsurname = firstsurname;
+    }
+
+    public String getSecondsurname() {
+        return secondsurname;
+    }
+
+    public void setSecondsurname(String secondsurname) {
+        this.secondsurname = secondsurname;
+    }
+
+    public int getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(int phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+}
