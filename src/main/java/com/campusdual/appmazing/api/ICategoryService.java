3676032e0d878994ef50b6578fd49d5bf85cc254
ptaibo <ptaibog@gmail.com>
2023-09-29 09:43:44 +0200

    Add ICategoryService, updated method queryCaterory(ies)

--- src/main/java/com/campusdual/appmazing/api/ICategoryService.java
+++ src/main/java/com/campusdual/appmazing/api/ICategoryService.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface ICategoryService {
 
-    CategoryDTO queryCategories (CategoryDTO category);
+    CategoryDTO queryCategory (CategoryDTO category);
 
     List<CategoryDTO> queryAllCategories();
 
