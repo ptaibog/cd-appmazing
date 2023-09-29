790cd7b056d1700ca9fea6e29b76a097c9a11519
ptaibo <ptaibog@gmail.com>
2023-09-29 09:19:24 +0200

    Add CategoryDTO

---
+++ src/main/java/com/campusdual/appmazing/model/dto/CategoryDTO.java
@@ -0,0 +1,23 @@
+package com.campusdual.appmazing.model.dto;
+
+public class CategoryDTO {
+
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
